ac4f411ab40627bc4efba04314eaa55c1245c9fb
vinc86 <vincenzomancuso86@gmail.com>
2020-02-24 21:55:27 +0100

    update

--- login.php
+++ login.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css" type="text/css">
-    <title>Login form</title>
-</head>
-<body>
+
+
+<?php require "./layout/header.php"; ?>
 
 
 <div class="container">
@@ -28,7 +22,5 @@
 
 </div>
 
+<?php require "./layout/footer.php"; ?>
 
-
-</body>
-</html>
--- signup.php
+++ signup.php
@@ -1,18 +1,5 @@
 
-<?php 
-session_start();
-?> 
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-    <title>Registration Form</title>
-</head>
-<body>
+<?php require "./layout/header.php"; ?>
 
     <div class="container">
 
@@ -40,5 +27,4 @@ session_start();
 
     </div>
     
-</body>
-</html>
+<?php require "./layout/footer.php";
